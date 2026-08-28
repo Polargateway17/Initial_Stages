8f811d8b3e61ee826e9853653705e9c31f4e705a
Aryan <iamaryannage2659@gmail.com>
2026-08-28 23:34:50 +0530

    Stash 1 + Stash 2

--- network1.cpp
+++ network1.cpp
@@ -1,4 +1,23 @@
+<<<<<<< Updated upstream
 /*this is before stash*/
 
 
 /*this is after the stash */
+=======
+#include <bits/stdc++.h>
+using namespace std;
+class network1 {
+    int data1;
+    int data2;
+    bool connected;
+    string name;
+public: 
+    void display() {
+        cout << "This is network1 class." << endl;
+    }
+    void calculate() {
+        cout << "Calculating..." << endl;
+        cout<<data1 + data2 << endl;
+    }
+};
+>>>>>>> Stashed changes
